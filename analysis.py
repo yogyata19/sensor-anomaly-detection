020d66e02d5bff264134713d16ee82105267d36b
yogyata19 <yogyatabhoriya1907@gmail.com>
2026-01-09 01:06:58 +0530

    Finalised modular anomaly detection with visualization

--- analysis.py
+++ analysis.py
@@ -1,36 +1,38 @@
 import pandas as pd
-df=pd.read_csv("data/sensor.csv")
-print(df.head(10))
-print(df.info())
-print(df.describe())
-print(df.isnull().sum())
-df['anomaly']=False
-#data driven anomaly detection
-temp_mean=df['temp'].mean()
-temp_std=df['temp'].std()
-temp_upper=temp_mean +2*temp_std
-temp_lower=temp_mean-2*temp_std
-df.loc[(df['temp']>temp_upper)|(df['temp']<temp_lower),'anomaly']=True
-print(df['anomaly'].value_counts())
-#Z-score calc for temp
-df["z_score_temp"]=(df['temp']-temp_mean)/temp_std
 import matplotlib.pyplot as plt
-#seperating  normal and anomalous points
-normal_data=df[df['anomaly']==False]
-anomaly_data=df[df['anomaly']==True]
-plt.figure(figsize=(10,5))
-#plot normal temperature
-plt.plot(normal_data['ts'],
+def load_data(path):
+    """Load sensor dataset from CSV file"""
+    return pd.read_csv(path)
+def detect_temperature_anomalies(df):
+    """Detect temperature anomalies using Z-score"""
+    temp_mean=df['temp'].mean()
+    temp_std=df['temp'].std()
+    df["z_score_temp"]=(df['temp']-temp_mean)/temp_std
+    df['anomaly'] = (df['z_score_temp'].abs() > 2)
+
+    return df
+def visualize_temp(df):
+    """Visualize normal and anomalous temperature readings"""
+    #seperating  normal and anomalous points
+    normal_data=df[df['anomaly']==False]
+    anomaly_data=df[df['anomaly']==True]
+    #plot normal temperature
+    plt.plot(normal_data['ts'],
          normal_data['temp'],
          label="normal",
-         color="green")
-#plot anomalies
-plt.scatter(anomaly_data['ts'],
+         color="red")
+    #plot anomalies
+    plt.scatter(anomaly_data['ts'],
             anomaly_data['temp'],
             color='yellow',
             label='Anomaly')
-plt.xlabel("Time")
-plt.ylabel("Temperature")
-plt.title("Temperature Sensor Data with Anomalies")
-plt.legend()
-plt.show() 
+    plt.xlabel("Time")
+    plt.ylabel("Temperature")
+    plt.title("Temperature Sensor Data with Anomalies")
+    plt.legend()
+    plt.show() 
+df=load_data("data/sensor.csv")
+df= detect_temperature_anomalies(df)
+visualize_temp(df)
+
+    
