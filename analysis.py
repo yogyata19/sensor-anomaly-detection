0e9158a00e48a97198576168aff4b33d5f291774
yogyata19 <yogyatabhoriya1907@gmail.com>
2026-01-09 00:36:41 +0530

    Added correct visualization for temperature anomalies

--- analysis.py
+++ analysis.py
@@ -14,3 +14,23 @@ df.loc[(df['temp']>temp_upper)|(df['temp']<temp_lower),'anomaly']=True
 print(df['anomaly'].value_counts())
 #Z-score calc for temp
 df["z_score_temp"]=(df['temp']-temp_mean)/temp_std
+import matplotlib.pyplot as plt
+#seperating  normal and anomalous points
+normal_data=df[df['anomaly']==False]
+anomaly_data=df[df['anomaly']==True]
+plt.figure(figsize=(10,5))
+#plot normal temperature
+plt.plot(normal_data['ts'],
+         normal_data['temp'],
+         label="normal",
+         color="green")
+#plot anomalies
+plt.scatter(anomaly_data['ts'],
+            anomaly_data['temp'],
+            color='yellow',
+            label='Anomaly')
+plt.xlabel("Time")
+plt.ylabel("Temperature")
+plt.title("Temperature Sensor Data with Anomalies")
+plt.legend()
+plt.show() 
