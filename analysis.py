cc527effacbb5f80e840555ecbf5f39256f4f8de
yogyata19 <yogyatabhoriya1907@gmail.com>
2026-01-09 22:35:19 +0530

    Add multi-sensor anomaly detection using Z-score

--- analysis.py
+++ analysis.py
@@ -2,37 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 def load_data(path):
     """Load sensor dataset from CSV file"""
+    
     return pd.read_csv(path)
-def detect_temperature_anomalies(df):
-    """Detect temperature anomalies using Z-score"""
-    temp_mean=df['temp'].mean()
-    temp_std=df['temp'].std()
-    df["z_score_temp"]=(df['temp']-temp_mean)/temp_std
-    df['anomaly'] = (df['z_score_temp'].abs() > 2)
+    df['ts']=pd.to_datetime(df['ts'],unit='s')
+def detect_anomalies(df,column):
+    """Detect sensor anomalies using Z-score"""
+    mean=df[column].mean()
+    std=df[column].std()
+    df[f'z_score_{column}']=(df[column]-mean)/std
+    df[f'anomaly_{column}'] = (df[f'z_score_{column}'].abs() > 3)
 
     return df
-def visualize_temp(df):
-    """Visualize normal and anomalous temperature readings"""
+def visualize_sensor(df,column):
+    """Visualize normal and anomalous  readings"""
     #seperating  normal and anomalous points
-    normal_data=df[df['anomaly']==False]
-    anomaly_data=df[df['anomaly']==True]
+    normal_data=df[df[f'anomaly_{column}']==False]
+    anomaly_data=df[df[f'anomaly_{column}']==True]
+    plt.figure(figsize=(10,5))
     #plot normal temperature
     plt.plot(normal_data['ts'],
-         normal_data['temp'],
+         normal_data[column],
          label="normal",
-         color="red")
+         color="blue")
     #plot anomalies
     plt.scatter(anomaly_data['ts'],
-            anomaly_data['temp'],
-            color='yellow',
+            anomaly_data[column],
+            color='red',
             label='Anomaly')
     plt.xlabel("Time")
-    plt.ylabel("Temperature")
-    plt.title("Temperature Sensor Data with Anomalies")
+    plt.ylabel(column.capitalize())
+    plt.title(f"{column.capitalize()} Sensor Data with Anomalies")
     plt.legend()
     plt.show() 
-df=load_data("data/sensor.csv")
-df= detect_temperature_anomalies(df)
-visualize_temp(df)
+if __name__=="__main__" :   
+    df=load_data("data/sensor.csv")
+df= detect_anomalies(df,'temp')
+df=detect_anomalies(df,'humidity')
+visualize_sensor(df,'humidity')
+visualize_sensor(df,'temp')
 
     
